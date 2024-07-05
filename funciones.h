0a8da531f026182a1cc32f263e7ae9b51fe3f2f7
Fede <fedeluismenendez@gmail.com>
2024-07-05 03:01:35 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -2,25 +2,21 @@
 #define FUNCION_H_INCLUDED
 
 int tirarDado();
+void escribirTirada (int v[], int tam);
 void generarTirada(int v[], int tam);
 void mostrarTirada(int v[], int tam);
+void DibujarCuadrados(int PosY, int PosX);
+void SombraDados(int PosX, int PosY);
+void DibujaDados(int dados[], int PosY,int PosX);
 
-///PUNTUACION
 int SumaDeDados (int v[], int tam);
-int Sexteto (int v[], int tam, int num);
-bool Sexteto0 (int v[], int tam);
-int Escalera (int v[], int tam);
+bool SextetodeX (int v[], int tam);
+bool Sexteto (int v[], int tam);
+bool Escalera (int v[], int tam);
 
-///GUIAS
-bool compararVectoresEnMismaPosicion(int vec1[], int vec2[], int tam);
-bool existeNumeroVector(int vec[], int tam, int num);
 void ordenarVector(int vec[], int tam);
-int contarNumerosRepetidos(int vec[], int tam, int num);
-bool verificarTodosRepetidos(int vec[], int tam);
+void Estadistica (int vec[], int tam);
 
-///DISEÑO
 void PressEnter();
 
-
 #endif // FUNCION_H_INCLUDED
-
