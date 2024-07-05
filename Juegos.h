92b3301e575dac6ce65222212a402d0e77b9b061
Fede <fedeluismenendez@gmail.com>
2024-07-05 02:58:33 -0300

    Update Juegos.h

--- Juegos.h
+++ Juegos.h
@@ -1,7 +1,11 @@
 #ifndef JUEGOS_H_INCLUDED
 #define JUEGOS_H_INCLUDED
 
-void ModoUnJugador();
-void ModoDosJugadores();
+using namespace std;
+
+void ModoUnJugadorSimulado(int &PuntEst, std::string &nombEst);
+void ModoUnJugador(int &PuntEst, std::string &nombEst);
+void ModoDosJugadores(int &PuntEst, std::string &nombEst);
+void ModoDosJugadoresSimulado(int &PuntEst, std::string &nombEst);
 
 #endif // JUEGOS_H_INCLUDED
